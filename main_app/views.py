1f3ba9919af682561849e694d479e8578ac0516d
ohziiilov3 <klbaskerville0520@gmail.com>
2024-07-24 11:31:04 -0700

    feat: add Cat model and update views to render data

--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from .models import Cat
+
 
 # Create your views here.
 
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name 
-        self.breed = breed 
-        self.description = description
-        self.age = age 
-
-
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
 # define home view
 def home(request):
     return render(request, 'home.html');
@@ -29,4 +15,35 @@ def about(request):
     return render(request, 'about.html');
 
 def cat_index(request):
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # Fake DataBase
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name 
+#         self.breed = breed 
+#         self.description = description
+#         self.age = age 
+# cats = [
+#     Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+#     Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+#     Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+#     Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+# ]
