2182c5e146f9554aacdce8d0dbadfb406cd50c82
ohziiilov3 <klbaskerville0520@gmail.com>
2024-07-24 12:02:47 -0700

    add: cat_details view, url, template

--- main_app/views.py
+++ main_app/views.py
@@ -19,6 +19,10 @@ def cat_index(request):
     return render(request, 'cats/index.html', {'cats': cats})
 
 
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
 
 
 
